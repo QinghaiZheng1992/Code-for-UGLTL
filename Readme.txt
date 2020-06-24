95d6ed61bbda471afb7786b8d37a8863df77ecda
Jlwu1992 <jlwu1992@pku.edu.cn>
2020-06-24 16:04:34 +0800

    Initial commit

---
+++ Readme.txt
@@ -0,0 +1,31 @@
+This is the code for the paper 'Unified Graph and Low-rank Tensor Learning for Multi-view Clustering' Published in AAAI 2020. Please direct run '*.m' file by MATLAB to test the performance. As we fix the random seed in the released code, the parameters might be slightly different. Due to the space limit, We just provide three datasets, including the UCI-Digits and the Scene-15 datasets. The COIL-20 dataset can be found at https://github.com/Jlwu1992/Code-for-ETLMSC If you are interested in other datasets, please feel free to contact me. Our code is based on the ETLMSC[1] and MLAN[2]. If you use our code, please cite our paper as well as these two other papers. Thanks.
+
+
+@article{UGLTL,
+  title={Unified Graph and Low-rank Tensor Learning for Multi-view Clustering},
+  author={Wu, Jianlong and Xie, Xingyu and Nie, Liqiang and Lin, Zhouchen and Zha, Hongbin},
+  journal={{IEEE} Transactions on Image Processing},
+  volume={28},
+  number={12},
+  pages={5910-5922},
+  year={2019},
+}
+
+[1]
+@article{ETLMSC,
+  title={Essential tensor learning for multi-view spectral clustering},
+  author={Wu, Jianlong and Lin, Zhouchen and Zha, Hongbin},
+  journal={{IEEE} Transactions on Image Processing},
+  volume={28},
+  number={12},
+  pages={5910-5922},
+  year={2019},
+}
+
+[2]
+@inproceedings{MLAN,
+  title={Multi-view clustering and semi-supervised classification with adaptive neighbours},
+  author={Nie, Feiping and Cai, Guohao and Li, Xuelong},
+  booktitle={AAAI Conference on Artificial Intelligence},
+  year={2017}
+}
---
+++ main_Scene.m
@@ -0,0 +1,148 @@
+clc;clear
+rand('seed',100);
+addpath('ClusteringMeasure')
+addpath('code_coregspectral')
+addpath('dataset')
+addpath('LRR')
+addpath('twist')
+addpath('funs')
+%% load dataset
+dataset='scene15.mat';
+load(dataset);
+Y=gt;
+X1=X1';
+X2=X2';
+X3=X3';
+numClust=length(unique(Y));
+num_views=3;
+data{1}=double(X1);
+data{2}=double(X2);
+data{3}=double(X3);
+
+tr_num=size(X1,1);
+X1=data{1};
+X2=data{2};
+X3=data{3};
+
+for v=1:3
+    X{v}=data{v};
+    [X{v}]=NormalizeData(X{v});
+    %X{v} = zscore(X{v},1);
+end
+ 
+ratio=1;
+sigma(1)=ratio*optSigma(X1);
+sigma(2)=ratio*optSigma(X2);
+sigma(3)=ratio*optSigma(X3);
+gt=Y;
+cls_num = numClust;
+
+%% Construct the initial similarity matrix
+K=[];
+T=cell(1,num_views);
+for j=1:num_views
+    options.KernelType = 'Gaussian';
+    options.t = sigma(j);
+    K(:,:,j) = constructKernel(data{j},data{j},options);
+    T{j}=K(:,:,j);
+%     D=diag(sum(K(:,:,j),2));
+%     L_rw=D^-1*K(:,:,j);
+%     T{j}=L_rw;
+end
+T_tensor = cat(3, T{:,:});
+t = T_tensor(:);
+V = length(data); N = size(data{1},1); %sample number
+all_dim=0;
+for k=1:V
+    all_dim=all_dim+size(X{k},2);
+    Z{k} = zeros(N,N); %Z{2} = zeros(N,N);
+%     W{k} = zeros(N,N);
+end
+Z_tensor = cat(3, Z{:,:});
+% W_tensor = cat(3, W{:,:});
+S_tensor=T_tensor;
+sX = [N, N, V];
+%
+H = eye(tr_num)-1/tr_num*ones(tr_num);
+% St = X*H*X';
+% invSt = inv(St);
+
+for ii=1:3
+    dim=size(X{ii},2);
+    invXTX{ii}=inv(10^(-6)*eye(dim,dim)+X{ii}'*H*X{ii});
+end
+% for ii=1:3
+%     H = eye(N)-1/N*ones(N);
+%     dim=size(X{ii},2);
+%     St = X{ii}'*H*X{ii};
+%     invXTX{ii}=inv(St);
+% end
+
+%% parameters setting
+iter = 0;
+tol = 1e-6;
+max_iter=20;
+d=8;
+W_cat_matrix=zeros(all_dim,d);
+alpha=30;
+beta=150;
+gamma=5;
+tao=beta/alpha;
+while iter < max_iter
+    fprintf('----processing iter %d--------\n', iter+1);
+    Zpre=Z_tensor;
+    Spre=S_tensor;
+    Wpre=W_cat_matrix;
+    s=S_tensor(:);
+    % update Z
+    [z, objV] = wshrinkObj(s,tao,sX,0,3)   ;
+    Z_tensor = reshape(z, sX);
+    Z{1}=Z_tensor(:,:,1);
+    Z{2}=Z_tensor(:,:,2);
+    Z{3}=Z_tensor(:,:,3);
+    % update W
+    for ii=1:num_views
+        tmp_S=S_tensor(:,:,ii);
+        tmp_D=diag(sum(tmp_S));
+        tmp_L=tmp_D-tmp_S;
+        M = invXTX{ii}*(X{ii}'*tmp_L*X{ii});
+%         tmp_W = eig1(M, d, 0, 0);
+        [tmp_W,~] = eigs(M, d);
+        W{ii} =tmp_W*diag(1./sqrt(diag(tmp_W'*tmp_W)));
+    end
+    W_cat_matrix = [W{1};W{2};W{3}];
+    
+    % update S
+    for ii=1:num_views
+        A=zeros(N);
+        distx = L2_distance_1(W{ii}'*X{ii}',W{ii}'*X{ii}');
+        [temp, idx] = sort(distx,2);
+        tmp_Z=Z{ii};
+        for jj=1:N
+%             idxa0 = 1:N;
+            dxi = distx(jj,:);
+            ad = -(dxi-alpha*tmp_Z(jj,:))/(alpha+gamma);
+            A(jj,:) = EProjSimplex_new(ad);
+        end
+        S_tensor(:,:,ii)=A;
+    end
+    % check convergence
+    tp_rank=rank(Z{1});
+    if tp_rank<1.2*numClust
+        break;
+    end
+    iter = iter+1;
+end
+    
+S = zeros(N,N);
+for k=1:num_views
+    S = S + Z{k};
+end
+S=(S+S')/2;
+
+C = SpectralClustering(S,cls_num);
+[A nmi avgent] = compute_nmi(gt,C);
+ACC = Accuracy(C,double(gt));
+[f,p,r] = compute_f(gt,C);
+[AR,RI,MI,HI]=RandIndex(gt,C);
+fprintf('tao=%f, F=%f, P=%f, R=%f, nmi score=%f, avgent=%f,  AR=%f, ACC=%f,\n',tao,f(1),p(1),r(1),nmi(1),avgent(1),AR(1),ACC(1));
